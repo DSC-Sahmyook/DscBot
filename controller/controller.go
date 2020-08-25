2bc4c7d63a81175721fc0986b94ff1d35015af64
parkjoungwan <whddhs6645@gmail.com>
2020-08-25 19:34:27 +0900

    update controller.go: item과 채널정보 명령어를 통해서 채널 정보 확인 및 업데이트 가능

--- controller/controller.go
+++ controller/controller.go
@@ -7,8 +7,93 @@ import (
 	"github.com/adlio/trello"
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/lib/pq"
-	"strconv"
+	"strings"
 )
+
+const (
+	DB_USER     = "dscbot"
+	DB_PASSWORD = "dscbot0215"
+	DB_NAME     = "dscbot"
+)
+
+//DBconnect for connect to postgresql
+func DBconnect(s *discordgo.Session, m *discordgo.MessageCreate, state int) {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	//vaildate postgrewql db
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	//create connection with Postgresql db
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	//create first input for channel_basic
+	if state == 1 {
+		message := m.Content
+		message = strings.Replace(message, "!item ", "", 1)
+		INFO := strings.Split(message, " ")
+		//first check
+
+		var Notfirst bool
+		err := db.QueryRow("select activate from channel_basic where channelid=$1", m.ChannelID).Scan(&Notfirst)
+		//not the first time
+		if Notfirst == true {
+			//sql for update
+			channelid := m.ChannelID
+			updatesql := `
+			UPDATE channel_basic
+			SET channelinfo = $1, trellourl = $2
+			WHERE channelid = $3
+			;`
+			//update info
+			result, err := db.Exec(updatesql, INFO[0], INFO[1], channelid)
+			if err != nil {
+				panic(err)
+			}
+			n, err := result.RowsAffected()
+			if n == 0 {
+				fmt.Println("0 row update")
+			}
+
+			s.ChannelMessageSend(m.ChannelID, "채널정보갱신")
+			//test: view changes
+			//DBconnect(s, m, 2)
+
+			return
+		} else { //first time
+			sqlStatement := `
+			INSERT INTO channel_basic (channelid,channelinfo,trellourl,activate)
+			VALUES ($1, $2, $3, true)`
+			channelid := m.ChannelID
+			_, err = db.Exec(sqlStatement, channelid, INFO[0], INFO[1])
+			if err != nil {
+				panic(err)
+			}
+			s.ChannelMessageSend(m.ChannelID, "추가완료")
+		}
+	}
+	//show info channelinfo && trellourl
+	if state == 2 {
+		//info is channelinfo
+		var info string
+		err := db.QueryRow("select channelinfo from channel_basic where channelid=$1", m.ChannelID).Scan(&info)
+		if err != nil {
+			panic(err)
+		}
+		//url is trellourl
+		var url string
+		err = db.QueryRow("select trellourl from channel_basic where channelid=$1", m.ChannelID).Scan(&url)
+		if err != nil {
+			panic(err)
+		}
+		//show info + url in discord
+		s.ChannelMessageSend(m.ChannelID, "chanenlinfo: "+info+"\n trellourl: "+url)
+	}
+}
 
 var Message string = "value"
 
@@ -41,21 +126,15 @@ func Board() string {
 }
 
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		Board()
-		s.ChannelMessageSend(m.ChannelID, Message)
-		s.ChannelMessageSend(m.ChannelID, Board())
+	// If the message has "!item" add info to DB
+	if strings.Contains(m.Content, "!item") {
+		DBconnect(s, m, 1)
 	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	// if the message has "!채널정보" show info in discord
+	if strings.Contains(m.Content, "!채널정보") {
+		DBconnect(s, m, 2)
 	}
 }
